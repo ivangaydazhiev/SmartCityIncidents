fbc11d2a1a30f058d555841ca234bece10f87d25
Иван <ivan.gaidajiev@abv.bg>
2026-03-25 14:25:37 +0200

    test: add unit tests for update operation and not-found scenario in IncidentService

--- SmartCity.Tests/IncidentServiceTests.cs
+++ SmartCity.Tests/IncidentServiceTests.cs
@@ -77,5 +77,72 @@ namespace SmartCity.Tests
                 .WithMessage("Title is required");
         }
 
+        [Fact]
+        public async Task UpdateAsync_Shoud_Update_Incident()
+        {
+            var id = Guid.NewGuid();
+
+            var existingIncident = new Incident
+            {
+                Id = id,
+                Title = "Old Title",
+                Description = "Old Desc",
+                Type = IncidentType.Fire,
+                Status = IncidentStatus.Reported,
+                LocationId = Guid.NewGuid(),
+                CreatedAt = DateTime.UtcNow
+            };
+
+            var dto = new UpdateIncidentDto
+            {
+                Title = "New Title",
+                Description = "New Desc",
+                Type = IncidentType.PowerOutage,
+                Status = IncidentStatus.Resolved,
+                LocationId = existingIncident.LocationId
+            };
+
+            _repositoryMock
+                .Setup(r => r.GetByIdAsync(id))
+                .ReturnsAsync(existingIncident);
+
+            _repositoryMock
+                .Setup(r => r.SaveChangesAsync())
+                .Returns(Task.CompletedTask);
+
+            var result = await _service.UpdateAsync(id, dto);
+
+            result.Title.Should().Be("New Title");
+            result.Description.Should().Be("New Desc");
+            result.Type.Should().Be((int)IncidentType.PowerOutage);
+            result.Status.Should().Be((int)IncidentStatus.Resolved);
+
+            _repositoryMock .Verify(r => r.Update(It.IsAny<Incident>()),Times.Once);
+            _repositoryMock.Verify(r => r.SaveChangesAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_Should_Throw_When_NotFound()
+        {
+            var id = Guid.NewGuid();
+
+            _repositoryMock
+                .Setup(r => r.GetByIdAsync(id))
+                .ReturnsAsync((Incident?)null);
+
+            var dto = new UpdateIncidentDto
+            {
+                Title = "Test",
+                Description = "Test",
+                Type = IncidentType.Fire,
+                Status = IncidentStatus.Reported,
+                LocationId = Guid.NewGuid()
+            };
+
+            Func<Task> act = async () => await _service.UpdateAsync(id, dto);
+
+            await act.Should().ThrowAsync<KeyNotFoundException>();
+        }
+
     }
 }
